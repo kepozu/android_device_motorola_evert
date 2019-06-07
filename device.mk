70173f74b507bfe5249d23b6a9e73edde1190c37
kana-ik <38389474+kepozu@users.noreply.github.com>
2019-06-07 20:38:45 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,6 +14,14 @@
 # limitations under the License.
 #
 
+# Inherit some common Lineage stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/motorola/evert/evert-vendor.mk)
 
